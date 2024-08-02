6ae5a0d70a54525911e9c176538c908ea98c482d
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-03 00:37:07 +1000

    Initial Commit

---
+++ WordleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordleApp
+//
+//  Created by Данил Аникин on 03.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
