0e5a6eb62c6a6dfe32f1d00b270155091adb25ff
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-27 11:39:47 +1000

    Initial Commit

---
+++ WordleApp/WordleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAppApp.swift
+//  WordleApp
+//
+//  Created by Данил Аникин on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
